8483ad29b49576e6b9be33c143f68eca7b71aa21
olegstabr <olegstabr@gmail.com>
2017-04-01 20:45:54 +0700

    create Cell & Grid classes

---
+++ Life/Life/LifeMainWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Life
+{
+    public partial class LifeMainWindow : Microsoft.Xna.Framework.Game
+    {
+        public static Vector2 ScreenSize;
+
+        public const int CellSize = 10; // Cell pixel width/height
+        public const int CellsX = 100;
+        public const int CellsY = 50;
+
+        public static Texture2D Pixel;
+
+        private GraphicsDeviceManager graphics;
+
+        public LifeMainWindow()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            ScreenSize = new Vector2(CellsX, CellsY) * CellSize;
+
+            graphics.PreferredBackBufferWidth = (int)ScreenSize.X;
+            graphics.PreferredBackBufferHeight = (int)ScreenSize.Y;
+
+
+            InitializeComponent();
+        }
+    }
+}
--- Life/Life/Program.cs
+++ Life/Life/Program.cs
@@ -16,7 +16,7 @@ namespace Life
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LifeMainWindow());
         }
     }
 }
